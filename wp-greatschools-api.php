98e0964750c59a8cb26b8fa5d7466db1004bc191
Brandon Hubbard <bkhubbard@gmail.com>
2016-08-22 11:49:16 -0700

    getting started

---
+++ wp-greatschools-api.php
@@ -0,0 +1,16 @@
+<?php
+/**
+* WP GreatSchools API
+*
+* @package WP-GreatSchools-API
+*/
+
+/* Exit if accessed directly */
+if ( ! defined( 'ABSPATH' ) ) { exit; }
+
+/**
+ * GreatSchools API Class.
+ */
+class GreatSchoolsAPI {
+
+}
